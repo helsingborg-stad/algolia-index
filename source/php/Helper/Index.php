e9f0fe9587f6d40dcab942a785780b69040c2524
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-05-12 10:34:40 +0200

    Send header details to algolia

--- source/php/Helper/Index.php
+++ source/php/Helper/Index.php
@@ -21,11 +21,21 @@ class Index
             return self::$_index;
         }
 
-        //Connect to account
-        $client = \Algolia\AlgoliaSearch\SearchClient::create(
+        //Setup config details with auth
+        $config = \Algolia\AlgoliaSearch\Config\SearchConfig::create(
             Options::applicationId(),
             Options::apiKey()
         );
+
+        //Tell algolia if running in cron and/or cli
+        $config->setDefaultHeaders([
+            'X-Client-Cli' => defined('WP_CLI_VERSION') ? WP_CLI_VERSION : 'false',
+            'X-Client-Cron' => defined( 'DOING_CRON' ) ? 'true' : 'false',
+            'X-Client-User' => get_current_user_id(),
+        ]);
+
+        //Init client with config
+        $client = \Algolia\AlgoliaSearch\SearchClient::createWithConfig($config);
 
         //Select index
         return self::$_index = $client->initIndex(Options::indexName());
