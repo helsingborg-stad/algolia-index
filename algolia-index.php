06112812e1b76218c51c5d13442477b13f7b5b21
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-01-14 16:14:32 +0100

    Update vendor fetch

--- algolia-index.php
+++ algolia-index.php
@@ -25,6 +25,17 @@ define('ALGOLIAINDEX_TEMPLATE_PATH', ALGOLIAINDEX_PATH . 'templates/');
 load_plugin_textdomain('algolia-index', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
 require_once ALGOLIAINDEX_PATH . 'vendor/autoload.php';
+
+// Autoload from plugin
+if (file_exists(ALGOLIAINDEX_PATH . 'vendor/autoload.php')) {
+    require_once ALGOLIAINDEX_PATH . 'vendor/autoload.php';
+}
+
+// Autoload from ABSPATH
+if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
+    require_once dirname(ABSPATH) . '/vendor/autoload.php';
+}
+
 require_once ALGOLIAINDEX_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
 require_once ALGOLIAINDEX_PATH . 'Public.php';
 
