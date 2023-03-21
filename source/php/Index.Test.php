c1c519834d1929656a7a4fb2d287a16bccb9324e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-21 16:31:14 +0100

    Test Index.php should index cases

--- source/php/Index.Test.php
+++ source/php/Index.Test.php
@@ -1,12 +1,63 @@
 <?php 
 
+use \AlgoliaIndex\Index as Index;
+
 class IndexTest extends WP_UnitTestCase
 {
-    public function testSomeThing()
-    {
-        // Given
-        // When
-        // Then
-        $this->assertTrue(true);
-    }
+
+  private $targetTestClass = null; 
+
+  public function __construct()
+  {
+    $this->targetTestClass = new Index();
+  }
+
+  public function invokeMethod(&$object, $methodName, array $parameters = array())
+  {
+      $reflection = new \ReflectionClass(get_class($object));
+      $method = $reflection->getMethod($methodName);
+      $method->setAccessible(true);
+
+      return $method->invokeArgs($object, $parameters);
+  }
+
+  public function testThatShouldIndexIsEnabledByDefault()
+  {
+      // Given
+      $post = self::factory()->post->create_and_get([
+        "post_title" => "Test Post"
+      ]);
+
+      // When
+      $isIndexable = $this->invokeMethod(
+        $this->targetTestClass,
+        'shouldIndex',
+        [$post]
+      );
+
+      // Then
+      $this->assertTrue($isIndexable);
+  }
+
+  public function testThatShouldIndexIsDisabledByMeta()
+  {
+      // Given
+      $post = self::factory()->post->create_and_get([
+        "post_title" => "Test Post",
+        "meta_input" => [
+          "exclude_from_search" => "1"
+        ]
+      ]);
+
+      // When
+      $isIndexable = $this->invokeMethod(
+        $this->targetTestClass,
+        'shouldIndex',
+        [$post]
+      );
+
+      // Then
+      $this->assertTrue($isIndexable);
+  }
+
 }
