f016bfb5413d3a4b2e2d110047b522dbb8a8b4eb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-03-21 15:08:12 +0100

    Update test files.

---
+++ source/php/App.Test.php
@@ -0,0 +1,15 @@
+<?php 
+
+use \AlgoliaIndex\Helper\Options as Options;
+
+class AppTest extends WP_UnitTestCase
+{
+    public function testThatisConfiguredReturnsFalseWhenNotConfigured()
+    {
+        // Given
+        $sut = Options::isConfigured();
+        
+        // Then
+        $this->assertFalse($sut);
+    }
+}
---
+++ source/php/Index.Test.php
@@ -0,0 +1,12 @@
+<?php 
+
+class IndexTest extends WP_UnitTestCase
+{
+    public function testSomeThing()
+    {
+        // Given
+        // When
+        // Then
+        $this->assertTrue(true);
+    }
+}
