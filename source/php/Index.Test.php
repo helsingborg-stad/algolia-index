3e47b914d6672d7e5386a9e922e1358f724800f4
Thor Brink <thor.brink@helsingborg.se>
2023-11-24 09:52:56 +0000

    test: fix broken test

--- source/php/Index.Test.php
+++ source/php/Index.Test.php
@@ -57,7 +57,7 @@ class IndexTest extends WP_UnitTestCase
       );
 
       // Then
-      $this->assertTrue($isIndexable);
+      $this->assertFalse($isIndexable);
   }
 
   public function testThatASmallRecordIsentTooLarge() {
