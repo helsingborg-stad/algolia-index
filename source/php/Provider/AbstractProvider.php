c25a48636a9da496156e4ec335cd75aacc6adbb4
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2025-05-02 10:45:57 +0200

    add AbstractProvider

---
+++ source/php/Provider/AbstractProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AlgoliaIndex\Provider;
+
+interface AbstractProvider
+{
+    public function setSettings(array $settings);
+
+    public function search(string $query);
+
+    public function clearObjects();
+
+    public function deleteObject(string $objectId);
+
+    public function deleteObjects(array $objectIds);
+
+    public function saveObject(array $object, array $options = []);
+
+    public function saveObjects(array $objects, array $options = []);
+
+    public function getObjects(array $objectIds);
+}
