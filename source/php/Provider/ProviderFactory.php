50376205d43de804b749249882ececa5d9c66a0b
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2025-05-19 12:39:40 +0200

    seperate provider registration and provider selection

--- source/php/Provider/ProviderFactory.php
+++ source/php/Provider/ProviderFactory.php
@@ -8,9 +8,25 @@ class ProviderFactory
 {
     public static function createFromEnv(): AbstractProvider
     {
-        return apply_filters(
-            "AlgoliaIndex/Provider/Factory", 
-            fn () => \AlgoliaIndex\Provider\Algolia\AlgoliaFactory::createFromEnv()
-        )();
+        $providers = apply_filters("AlgoliaIndex/Provider/Factory", [
+            'algolia' => fn () => \AlgoliaIndex\Provider\Algolia\AlgoliaFactory::createFromEnv()    
+        ]);
+
+        $provider = apply_filters('AlgoliaIndex/Provider', 'algolia', $providers);
+
+        if (!is_string($provider)) {
+            throw new \InvalidArgumentException('Provider name must be a string');
+        }
+
+        if (!array_key_exists($provider, $providers)) {
+            throw new \InvalidArgumentException('Provider not found');
+        }
+        if (!is_callable($providers[$provider])) {
+            throw new \InvalidArgumentException('Provider is not callable');
+        }
+
+        $factory = $providers[$provider];
+
+        return $factory();
     }
 }
