06f5310c0dfcf43684f16ebb3de3c8767553a504
Petter Andersson <petter.andersson@helsingborg.se>
2025-11-12 10:48:00 +0100

    initial commit

---
+++ vite.config.mjs
@@ -0,0 +1,10 @@
+import { createViteConfig } from "vite-config-factory";
+
+const entries = {
+        'css/algolia-index': './source/sass/algolia-index.scss',
+        'js/algolia-index':  './source/js/algolia-index'};
+
+export default createViteConfig(entries, {
+	outDir: "assets/dist",
+	manifestFile: "manifest.json",
+});
